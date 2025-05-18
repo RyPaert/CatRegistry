08c2b99ecb3cbd6388d2ae47819706c3740b7d47
Ry <thomashenrypaert@gmail.com>
2025-05-18 13:40:52 +0300

    https://sarksandra9.atlassian.net/browse/CAT-41
    I added KittyContext and a DbInitializer to populate the db. I have yet to make the views so I have not been able to test it.

---
+++ CatRegistry/Data/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using CatRegistry.Domain;
+
+namespace CatRegistry.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(KittyContext context)
+        {
+            if(context.Kittys.Any())
+            {
+                return;
+            }
+
+            var kittys = new Kitty[]
+            {
+                new Kitty()
+                {
+                    KittyId = Guid.NewGuid(),
+                    KittySpeciesName = "European Wildcat",
+                    KittyDescription = "The European wildcat (Felis silvestris) is a small wildcat species native to continental Europe, Great Britain, Turkey and the Caucasus. \n" +
+                                       "Its fur is brownish to grey with stripes on the forehead and on the sides and has a bushy tail with a black tip. \n" +
+                                       "It reaches a head-to-body length of up to 65 cm (26 in) with a 34.5 cm (13.6 in) long tail, and weighs up to 7.5 kg (17 lb).",
+                    KittyRegionOfOrigin = "Europe"
+                }
+            };
+        }
+    }
+}
---
+++ CatRegistry/Data/KittyContext.cs
@@ -0,0 +1,18 @@
+﻿using CatRegistry.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CatRegistry.Data
+{
+    public class KittyContext : DbContext
+    {
+        public KittyContext(DbContextOptions<KittyContext> options) : base(options) { }
+            public DbSet<Kitty> Kittys { get; set; }
+            public DbSet<FileToDatabase> FileToDatabase {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Kitty>().ToTable("Kittys")
+        }
+    }
+}
